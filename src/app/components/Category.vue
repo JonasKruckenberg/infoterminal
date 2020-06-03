7749b56e2fae3e38ade2334aa7f273b3d6d580d0
Jonas Kruckenberg <jonas@kruckenbg.de>
2020-06-03 17:21:53 +0200

    feat: :sparkles: Add markdown descriptions

--- src/app/components/Category.vue
+++ src/app/components/Category.vue
@@ -1,101 +1,128 @@
 <template>
-  <router-link class="category" :class="{ highlight: center}" :to="link">{{title}}</router-link>
+	<div class="category-wrapper">
+		<router-link
+			class="category"
+			:data-background-image="thumbnail"
+			:class="{ highlight: center }"
+			:to="link"
+		>
+			<h2>{{ title }}</h2>
+			<div class="wrapper">
+				<span class="description" v-if="center" v-html="description"></span>
+				<div class="button" type="button">Mehr erfahren</div>
+			</div>
+		</router-link>
+	</div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
-import { Location } from "vue-router";
-import lozad from "lozad";
-import { threadId } from "worker_threads";
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Location } from 'vue-router';
+import lozad from 'lozad';
+import { threadId } from 'worker_threads';
 
 @Component
 export default class Category extends Vue {
-  @Prop()
-  parent: string;
-  @Prop()
-  title: string;
-  @Prop({ default: "media" })
-  type: string;
-  @Prop()
-  thumbnail: string;
+	@Prop()
+	parent: string;
+	@Prop()
+	title: string;
+	@Prop({ default: 'media' })
+	type: string;
+	@Prop()
+	thumbnail: string;
+	@Prop()
+	description: string;
 
-  center: boolean = false;
-  centerObserver: IntersectionObserver;
+	center: boolean = false;
+	lastIntersection: number = 0;
+	centerObserver: IntersectionObserver;
 
-  mounted() {
-    let options: IntersectionObserverInit = {
-      trackVisibility: false,
-      root: null,
-      rootMargin: `0px -${document.body.clientWidth / 2 - 180}px`,
-      threshold: 720 / window.innerHeight
-    };
+	mounted() {
+		let options: IntersectionObserverInit = {
+			root: null,
+			rootMargin: `0px -${document.body.clientWidth / 2 - 300}px`,
+			threshold: 1
+		};
 
-    this.centerObserver = new IntersectionObserver(entries => {
-      if (this.center !== entries[0].isIntersecting)
-        this.center = entries[0].isIntersecting;
+		this.centerObserver = new IntersectionObserver(entries => {
+			this.center = entries[0].isIntersecting;
+		}, options);
+		this.centerObserver.observe(this.$el);
 
-      // this.center = entries[0].isIntersecting === this.center;
-    }, options);
-    this.centerObserver.observe(this.$el);
-  }
-  destroyed() {
-    this.centerObserver.disconnect();
-  }
+		const observer = lozad(this.$el.children[0]);
+		observer.observe();
+	}
+	destroyed() {
+		this.centerObserver.disconnect();
+	}
 
-  get link() {
-    const out: Location = { params: { path: "" } };
-    if (this.type === "category") out.name = "Categories";
-    if (this.type === "map") out.name = "Map";
-    if (this.type === "media") out.name = "Preview";
-    if (this.parent) out.params.path += this.parent + ".";
-    out.params.path += this.title;
-    return out;
-  }
+	get link() {
+		const out: Location = { params: { path: '' } };
+		if (this.type === 'category') out.name = 'Categories';
+		if (this.type === 'map') out.name = 'Map';
+		if (this.type === 'media') out.name = 'Preview';
+		if (this.parent) out.params.path += this.parent + '.';
+		out.params.path += this.title;
+		return out;
+	}
 }
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/variables.scss";
+@import '@/assets/variables.scss';
+.category-wrapper {
+	scroll-snap-align: center;
+	display: inline-block;
+	width: 40vh;
+	height: 60vh;
+	padding: 1.5rem;
+	transform: translateY(-50%);
+	margin-top: 50vh;
+	vertical-align: bottom;
+}
 .category {
-  scroll-snap-align: center;
-  width: 9rem * 2.5;
-  height: 13rem * 2.5;
-  margin: 1rem;
-  flex-shrink: 0;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-  border-radius: $rounding;
-  background-repeat: no-repeat;
-  background-color: green;
-  // background-size: cover;
-  // background-position: center;
-  // background-color: rgba($black, 0.4);
-  // background-blend-mode: darken;
-  transform: scale(1);
-  transition: transform 200ms, opacity 300ms, position 300ms, height 200ms;
-  color: $white;
-  text-decoration: none;
-  h2 {
-    text-align: center;
-  }
-  .button {
-    opacity: 0;
-    background-color: $primary;
-    flex-grow: 1;
-  }
-  &:focus {
-    outline: none;
-  }
+	border-radius: $rounding;
+	white-space: initial;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	flex-wrap: nowrap;
+	width: 100%;
+	height: 100%;
+	transition: transform 200ms, background-color 200ms;
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center;
+	background-color: rgba($black, 0.3);
+	background-blend-mode: darken;
+	background-image: url('../assets/placeholder.svg');
+	text-align: center;
+	text-decoration: none;
+	overflow-y: auto;
+	h2 {
+		display: block;
+	}
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		opacity: 0;
+		transition: opacity 250ms;
+		.button {
+			text-decoration: underline;
+			background-color: $primary;
+			flex-grow: 1;
+		}
+	}
 }
 .highlight {
-  color: red;
-  box-shadow: $shadow-small;
-  transform: scale(1.1);
-  .button {
-    opacity: 1;
-  }
+	transform: scale(1.1);
+	background-color: rgba($black, 0.5);
+	.wrapper {
+		span {
+			transform: scaleY(1);
+		}
+		opacity: 1;
+	}
 }
 </style>
--- src/app/pages/Preview.vue
+++ src/app/pages/Preview.vue
@@ -1,8 +1,62 @@
-<template></template>
+<template>
+	<div class="page">
+		<div class="preview" :data-background-image="thumbnail">
+			<h2>{{ title }}</h2>
+			<span class="description" v-html="description"></span>
+		</div>
+	</div>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import marked from 'marked';
+import DOMPurify from 'dompurify';
+import lozad from 'lozad';
+const { ipcRenderer } = window;
+
+@Component
+export default class Preview extends Vue {
+	@Prop({ required: true })
+	path: string;
+
+	title: string = this.path.split('.').pop();
+	description: string = '';
+	thumbnail: string = '';
+
+	async mounted() {
+		await this.fetchData();
+		const observer = lozad(this.$el.children[0]);
+		observer.observe();
+	}
+
+	async fetchData() {
+		const data = await ipcRenderer.invoke('media.get', this.path);
+		this.description = DOMPurify.sanitize(marked(data.description || ''));
+		this.thumbnail = data.thumbnail;
+	}
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/assets/variables.scss';
+.page {
+	padding: 10vh 5vw;
+}
+.preview {
+	width: 100%;
+	height: 100%;
+	border-radius: $rounding;
+	padding: 1rem;
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center;
+	background-color: rgba($black, 0.6);
+	background-blend-mode: darken;
+	h2 {
+		margin-bottom: 1rem;
+	}
+	.description {
+		font-size: 1.2rem;
+	}
+}
 </style>
